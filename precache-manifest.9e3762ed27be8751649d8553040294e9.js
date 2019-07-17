cb608cdbd86134b2018176ef9e63799d01f7c995
nyowusu <yaw.owusu@hotmail.com>
2019-07-17 16:20:57 +0100

    Updates

---
+++ precache-manifest.9e3762ed27be8751649d8553040294e9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb6125ae999dbbd47e6fc4bf4971c297",
+    "url": "/scalc/index.html"
+  },
+  {
+    "revision": "8d877123b3aff22127c3",
+    "url": "/scalc/static/css/main.9b157bc2.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/scalc/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "8d877123b3aff22127c3",
+    "url": "/scalc/static/js/main.748bc888.chunk.js"
+  },
+  {
+    "revision": "572050e986d0133b0aa9",
+    "url": "/scalc/static/js/runtime~main.3df0e811.js"
+  }
+]);
